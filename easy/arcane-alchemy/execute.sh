28cf92e194f278b42689ced237f8114ca36ae8f5
Akalanka <akalankaperera128@gmail.com>
2023-09-18 02:21:48 +0530

    Feat: added arcane alchemy

---
+++ easy/arcane-alchemy/execute.sh
@@ -0,0 +1,12 @@
+if [ ! -d "./src" ]; then
+  echo "No data to convert. Exiting..."
+  exit 0
+fi
+
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
+
+file=$(basename -- $(find src -name "*.xml") .xml)
+
+rm -rf dist && mkdir -p dist
+
+yq --input-format xml --output-format json ./src/$file.xml >> dist/$file.json
